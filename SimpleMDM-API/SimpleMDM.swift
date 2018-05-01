859701dc08f933a87f7feaa95c17986a6a334b8d
Guillaume Algis <guillaume.algis@gmail.com>
2018-05-01 17:35:51 +0200

    Add minimal code to test static library

---
+++ SimpleMDM-API/SimpleMDM.swift
@@ -0,0 +1,14 @@
+//
+//  SimpleMDM.swift
+//  SimpleMDM
+//
+//  Created by Guillaume Algis on 01/05/2018.
+//  Copyright © 2018 Guillaume Algis. All rights reserved.
+//
+
+public class SimpleMDM {
+    public static var APIKey: String! = nil
+
+    static let host = "a.simplemdm.com"
+    static let endpoint = "api/v1"
+}
