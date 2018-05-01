90cf2f9e57fe8ad2e12cce84a00524a8d77fa7b3
Guillaume Algis <guillaume.algis@gmail.com>
2018-05-01 16:45:47 +0200

    Initial commit

---
+++ SimpleMDM-API/SimpleMDM_API.swift
@@ -0,0 +1,11 @@
+//
+//  SimpleMDM_API.swift
+//  SimpleMDM-API
+//
+//  Created by Guillaume Algis on 01/05/2018.
+//  Copyright © 2018 Guillaume Algis. All rights reserved.
+//
+
+class SimpleMDM_API {
+
+}
