74f9603503f777568c988e2f925202c48b2706a9
Guillaume Algis <guillaume.algis@gmail.com>
2018-06-27 23:37:27 +0200

    Merge macOS and iOS targets into a unique cross-platform framework target
    And add support for watchOS and tvOS

---
+++ Sources/SimpleMDM.h
@@ -0,0 +1,16 @@
+//
+//  SimpleMDM.h
+//  SimpleMDM
+//
+//  Created by Guillaume Algis on 23/06/2018.
+//  Copyright © 2018 Guillaume Algis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMDM.
+FOUNDATION_EXPORT double SimpleMDMVersionNumber;
+
+//! Project version string for SimpleMDM.
+FOUNDATION_EXPORT const unsigned char SimpleMDMVersionString[];
+
