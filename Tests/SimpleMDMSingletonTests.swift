f714005c822042d797813cd67a422631b1390b71
Guillaume Algis <guillaume.algis@gmail.com>
2018-10-27 18:06:02 +0200

    Add tests for SimpleMDM singleton usage

---
+++ Tests/SimpleMDMSingletonTests.swift
@@ -0,0 +1,259 @@
+//
+//  Copyright 2018 Guillaume Algis.
+//  Licensed under the MIT License. See the LICENSE.md file in the project root for more information.
+//
+
+@testable import SimpleMDM
+import XCTest
+
+/// This class tests weither the exposed API using the SimpleMDM singleton calls to the right Networking instance.
+/// We do this by using a networking mock which returns a nonsensical HTTP code, and check in the callback we got
+/// this code as expected.
+internal class SimpleMDMSingletonTests: XCTestCase {
+    override static func setUp() {
+        let session = URLSessionMock(responseCode: 999)
+        let networking = Networking(urlSession: session)
+        networking.APIKey = "AGlobalAndRandomAPIKey"
+        SimpleMDM.shared.networking = networking
+    }
+
+    func testUniqueResourceGetViaSingleton() {
+        let expectation = self.expectation(description: "Callback called")
+
+        UniqueResourceMock.get { result in
+            guard case let .failure(error) = result else {
+                return XCTFail("Expected .error, got \(result)")
+            }
+            guard let simpleMDMError = error as? SimpleMDMError else {
+                return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+            }
+            guard case let .unknown(httpCode) = simpleMDMError else {
+                return XCTFail("Expected .unknown, got \(simpleMDMError)")
+            }
+            XCTAssertEqual(httpCode, 999)
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetAllViaSingleton() {
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceMock.getAll { result in
+            guard case let .failure(error) = result else {
+                return XCTFail("Expected .error, got \(result)")
+            }
+            guard let simpleMDMError = error as? SimpleMDMError else {
+                return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+            }
+            guard case let .unknown(httpCode) = simpleMDMError else {
+                return XCTFail("Expected .unknown, got \(simpleMDMError)")
+            }
+            XCTAssertEqual(httpCode, 999)
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetByIdViaSingleton() {
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceMock.get(id: 42) { result in
+            guard case let .failure(error) = result else {
+                return XCTFail("Expected .error, got \(result)")
+            }
+            guard let simpleMDMError = error as? SimpleMDMError else {
+                return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+            }
+            guard case let .unknown(httpCode) = simpleMDMError else {
+                return XCTFail("Expected .unknown, got \(simpleMDMError)")
+            }
+            XCTAssertEqual(httpCode, 999)
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetRelatedToOneViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toOne.get { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetAllRelatedToManyViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toMany.getAll { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetRelatedToManyAtIndexViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toMany.get(at: 0) { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetRelatedToManyByIdViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toMany.get(0) { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetAllRelatedToManyNestedViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toManyNested.getAll { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+
+    func testResourceGetRelatedToManyNestedByIdViaSingleton() {
+        let json = loadFixture("ResourceWithRelationsMock_42")
+        let session = URLSessionMock(data: json, responseCode: 200)
+        let s = SimpleMDM(sessionMock: session)
+
+        let expectation = self.expectation(description: "Callback called")
+
+        ResourceWithRelationsMock.get(s.networking, id: 42) { result in
+            guard case let .success(resource) = result else {
+                return XCTFail("Expected .success, got \(result)")
+            }
+
+            resource.toManyNested.get(0) { relationResult in
+                guard case let .failure(error) = relationResult else {
+                    return XCTFail("Expected .error, got \(relationResult)")
+                }
+                guard let simpleMDMError = error as? SimpleMDMError else {
+                    return XCTFail("Expected error to be a SimpleMDMError, got \(error)")
+                }
+                guard case let .unknown(httpCode) = simpleMDMError else {
+                    return XCTFail("Expected .unknown, got \(simpleMDMError)")
+                }
+                XCTAssertEqual(httpCode, 999)
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+}
--- Tests/Tools/ResourceMock.swift
+++ Tests/Tools/ResourceMock.swift
@@ -76,3 +76,40 @@ internal struct ResourceWithDateMock: ListableResource {
         try decodeAndValidateType(forKey: .type, in: payload, expecting: "resource_with_date_mock")
     }
 }
+
+/// An listable resource with a field of type `Date` used when testing.
+internal struct ResourceWithRelationsMock: ListableResource {
+    typealias Identifier = Int
+
+    static let endpointName = "resource_with_relations_mock"
+
+    let id: Int
+    let toOne: RelatedToOne<ResourceMock>
+    let toMany: RelatedToMany<ResourceMock>
+    let toManyNested: RelatedToManyNested<ResourceWithRelationsMock, ResourceMock>
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case id
+        case attributes
+        case relationships
+    }
+
+    private enum RelationshipKeys: String, CodingKey {
+        case toOne
+        case toMany
+    }
+
+    init(from decoder: Decoder) throws {
+        let payload = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try payload.decode(Identifier.self, forKey: .id)
+
+        let relationships = try payload.nestedContainer(keyedBy: RelationshipKeys.self, forKey: .relationships)
+        toOne = try relationships.decode(RelatedToOne<ResourceMock>.self, forKey: .toOne)
+        toMany = try relationships.decode(RelatedToMany<ResourceMock>.self, forKey: .toMany)
+        toManyNested = RelatedToManyNested<ResourceWithRelationsMock, ResourceMock>(parentId: id)
+
+        try decodeAndValidateType(forKey: .type, in: payload, expecting: "resource_with_relations_mock")
+    }
+}
